04a79c6c812ec18b067359f419ff070010f22df4
Alexis <alex10o@hotmail.com>
2023-06-24 16:39:52 -0300

    se modifco el titulo de inicio serie y peliculas

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,10 +4,13 @@ import { MainContextState } from "@/components/context/MainContextProvider";
 const Home = () => {
     const { state } = useContext(MainContextState);
     
+    const newLocal = "m-0 p-2 text-center text-white bg-dark border rounded border-warning text-capitalize";
     return (
+        <> 
         <div className= "bg-dark">        
-        <h1 className="m-0 p-2 text-center text-white bg-dark border rounded border-warning">Bienvenido {state.username}</h1>
+        <h1 className={newLocal}>Bienvenido {state.username}</h1>
         </div>
+        </>
     )
 }
 
