2ffdf265745d184de95862e5b62062bbee7e6d8b
Miriam <73286285+arialcoder@users.noreply.github.com>
2023-07-01 21:27:17 -0300

    Update buscar.js: borre 2 lineas repetidas

--- src/pages/buscar.js
+++ src/pages/buscar.js
@@ -1,6 +1,3 @@
-import { useState, useEffect } from 'react'
-import { SearchList } from '@/components/search/SearchList'
-
 import { useState, useEffect } from "react";
 import { SearchList } from '@/components/search/SearchList';
 
